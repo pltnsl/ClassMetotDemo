de134ce807d93c2bad1444f1ffd0bc188d3b14b9
neslihan pulat <pulat_neslihan@hotmail.com>
2021-04-17 23:30:27 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id +" " + musteri.Adi +" " + musteri.Soyadi  + " " + musteri.Adresi  + " " + musteri.Meslegi + " " + "Müşteri Eklendi");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id +" " + musteri.Adi +" " +  musteri.Soyadi +" " +  musteri.Adresi + " " + musteri.Meslegi + " " + "Müşteri Silindi");
+        }
+
+       
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Necdet";
+            musteri1.Soyadi = "PAMUK";
+            musteri1.Adresi = "Bayır Sk.No:15 ANKARA";
+            musteri1.Meslegi = "Öğretmen";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Melda";
+            musteri2.Soyadi = "SAVAŞ";
+            musteri2.Adresi = "Güneş Sk.No:1 BURSA";
+            musteri2.Meslegi = "Mühendis";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri2);
+
+
+
+
+
+        }
+         
+
+
+    }
+}
